e29163dd8867dd9d925952cb4e5c006ee1e79577
LucasBruno2022 <111883074+LucasBruno2022@users.noreply.github.com>
2026-04-22 23:08:45 -0300

    Paso 7 .connect() manualmente

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -3,13 +3,16 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
+var GeneRojo
+var GeneVioleta
+
 
 func _ready():
-	# Inicializa el texto de "CountLabel" usando el texto
-	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
+	GeneRojo = get_node("RedGenerator").button_down.connect(count_new_instance)
+	GeneVioleta = get_node("VioletGenerator").button_down.connect(count_new_instance)
 	pass
 
 func count_new_instance():
 	instancesCount += 1
-	$CountLabel.text = str(instancesCount)
+	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
